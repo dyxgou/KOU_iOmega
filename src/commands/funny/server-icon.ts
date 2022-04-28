ec4caab4b0a66050cd8ed06cf02400c9ea6f5782
iDiego5614 <diego5614yt@gmail.com>
2022-04-27 22:34:33 -0500

    feat(Root): 🖼️ Server-Icon Command

---
+++ src/commands/funny/server-icon.ts
@@ -0,0 +1,22 @@
+import { Message } from "discord.js";
+import { ICallback } from "utils/Command";
+import { commonEmbed } from "../../utils/embeds";
+
+export default {
+  callback : (message : Message , ...args : string[]) => 
+  {
+    const serverImage = message.guild?.iconURL({ dynamic : true , size : 1024 })
+    
+    if(!serverImage)
+      return
+
+    const embed = commonEmbed(message)
+      .setAuthor({ name : `${message.guild?.name}'s Icon` })
+      .setImage(serverImage)
+      .setFooter({
+        text : "Re fachero  😎"
+      })
+
+    return message.reply({ embeds : [ embed ] })
+  }
+} as ICallback
