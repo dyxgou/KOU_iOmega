6c9e39fcf021c635af9e39906506ea4644ac9d5e
iDiego5614 <diego5614yt@gmail.com>
2022-04-29 23:19:25 -0500

    feat(Root): 🗺️ Nation Population Command Part 0°

---
+++ src/commands/nations/na-population.ts
@@ -0,0 +1,9 @@
+import { Message } from 'discord.js';
+import { ICallback } from 'utils/Command';
+
+export default {
+  callback : (message : Message , ...args: string[]) => 
+  {
+    
+  }
+} as ICallback
