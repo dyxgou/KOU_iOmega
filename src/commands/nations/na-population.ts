391a598d732aa20cc26ba46979d063ab72b56596
iDiego5614 <diego5614yt@gmail.com>
2022-04-30 22:48:50 -0500

    feat(Root): 🗺️ nuation Population part 2°

--- src/commands/nations/na-population.ts
+++ src/commands/nations/na-population.ts
@@ -1,9 +1,13 @@
 import { Message } from 'discord.js';
+import NationSchema from 'schemas/NationSchema';
 import { ICallback } from 'utils/Command';
+import { getUserInfo } from 'utils/getInformation';
 
 export default {
-  callback : (message : Message , ...args: string[]) => 
+  callback : async(message : Message , ...args: string[]) => 
   {
-    
+    const userInfo = getUserInfo(message)
+
+    const nation = await NationSchema.findOne(userInfo)    
   }
 } as ICallback
