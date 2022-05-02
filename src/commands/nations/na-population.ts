8be7af3ad50fb9f3208f03c547708573023e2d8d
iDiego5614 <diego5614yt@gmail.com>
2022-05-02 16:12:39 -0500

    feat(Root): 🗺️ Nation Population
    🗺️ Nation Population Command has been finished

--- src/commands/nations/na-population.ts
+++ src/commands/nations/na-population.ts
@@ -1,24 +1,45 @@
 import { Message } from 'discord.js';
 import NationSchema from '../../schemas/NationSchema';
 import { ICallback } from 'utils/Command';
-import { commonEmbed, notNationsFound } from '../../utils/embeds';
+import { commonEmbed, notNationsFound, serverNotFoundEmbed } from '../../utils/embeds';
 import { getUserInfo } from '../../utils/getInformation';
+import randomPopulationIncrement from '../../utils/populationIncrement';
+import GuildSchema from '../../schemas/GuildSchema';
 
 export default {
   callback : async(message : Message , ...args: string[]) => 
   {
     const userInfo = getUserInfo(message)
 
-    const nation = await NationSchema.findOne(userInfo,  {
-      nationExtention :  true , guildId : true
-    }).populate("guildId" , {
-      populationIncrement  : true
-    })
+    const [ nation , server ] = await Promise.all([
+      NationSchema.findOne(userInfo , { nationPopulation : true , nationExtention : true }),
+      GuildSchema.findOne({ guildId : userInfo.guildId } , { populationIncrement  : true ,  _id : false })
+    ])
 
     if(!nation)
       return message.reply({ embeds : [ notNationsFound(message) ] })
+    if(!server)
+      return message.reply({ embeds : [ serverNotFoundEmbed(message) ] })
 
-    
+    const { populationIncrement } = server
 
+    const newPopulation = randomPopulationIncrement({ populationIncrement , nation })
+    const embed = commonEmbed(message)
+
+    try {
+      await nation.updateOne({
+        $inc : {
+          nationPopulation : newPopulation
+        }
+      })
+
+      embed.setDescription(`Ahora hay \`${newPopulation}\` nuevas personas en tu creciente imperio. 😊`)
+    } catch (error) {
+      console.error({ error })
+
+      embed.setDescription(`Estaba pasado un dinosaurio que por error se comió a todos tus nuevos habitantes. 😭`)
+    }
+
+    return message.reply({ embeds : [ embed ] })
   }
 } as ICallback
---
+++ src/utils/populationIncrement.ts
@@ -0,0 +1,24 @@
+import { randomInt } from "mathjs"
+import { INation } from "schemas/NationSchema"
+import { MIN_POPULATION_INCREMENT } from "./defaultServerVars"
+interface PopulationIncrement 
+{ 
+  populationIncrement ?: number,
+  nation : INation
+}
+
+const randomPopulationIncrement = ({ populationIncrement = MIN_POPULATION_INCREMENT , nation } : PopulationIncrement) => 
+{
+  const maxPopulation = nation.nationExtention * populationIncrement
+  const popIncrement  = nation.nationPopulation + populationIncrement
+
+  if(popIncrement > maxPopulation)
+    return 0
+  
+  const newPopulation = randomInt(1 , popIncrement)
+
+  return newPopulation
+}
+
+
+export default randomPopulationIncrement
